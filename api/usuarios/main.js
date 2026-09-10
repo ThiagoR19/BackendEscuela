c129e1d8135fc755e78a3695582dff26fedb5bf5
Thiago_19 Notebook <riffothiago19@gmail.com>
2026-09-10 20:24:17 -0300

    feat: add put and delete

--- api/usuarios/main.js
+++ api/usuarios/main.js
@@ -31,12 +31,36 @@ router.post('/', (req, res, next) => {
     })
 })
 
-router.put('/', (req, res, next) => {
-  res.send('ok')
+router.put('/:usuario_id', (req, res, next) => {
+  const { usuario_id } = req.params
+  const { nombre, user, pass } = req.body
+
+  const SQL = 'update usuarios set nombre = ?, user = ?, pass = ? where id = ?'
+
+  db.query(SQL, [nombre, user, pass, usuario_id])
+    .then(([result, campos]) => {
+      console.log(result, " ", campos)
+      res.status(200).send('Usuario modificado')
+    })
+    .catch((e) => {
+      console.error(e)
+      res.status(500).json({ e })
+    })
 })
 
-router.delete('/', (req, res, next) => {
-  res.send('ok')
+router.delete('/:usuario_id', (req, res, next) => {
+  const { usuario_id } = req.params
+  const SQL = 'delete from usuarios where id = ?'
+
+  db.query(SQL, [usuario_id])
+    .then(([result, campos]) => {
+      console.log(result, " ", campos)
+      res.status(204).send('Usuario eliminado')
+    })
+    .catch((e) => {
+      console.error(e)
+      res.status(500).json({ e })
+    })
 })
 
 module.exports = router
